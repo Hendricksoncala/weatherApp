2291d3ed69215796df4d4d02ec47339c3428024c
Hendricksoncala <156349949+Hendricksoncala@users.noreply.github.com>
2024-10-21 23:11:39 -0500

    feat: ✨ creacion de llas posibles rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import Clima from "./components/Clima"
-import Stadistics from "./components/DashboardStas"
-import Header from "./components/Header"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TodayForecast from './components/views/TodayForecast';
+import TenDaysForecast from './components/views/TenDaysForecast.jsx';
+// import DayDetails from './components/views/DayDetails';
 
 function App() {
-
   return (
-    <div className="bg-[#f6edff]">
-      <main className="mx-auto container ">
-        <Header />
-        <Stadistics />
-        <Clima/>
-      </main>
-    </div>
-  )
+    <Router>
+      <Routes>
+        {/* Ruta principal, previsión de hoy */}
+        <Route index element={<TodayForecast/>} />
+        <Route path='/' element={<TodayForecast />} />
+        {/* Ruta de previsión a 10 días */}
+        <Route path="/10-days" element={<TenDaysForecast/>} />
+        {/*         
+        {/* Ruta para los detalles de un día específico */}
+        {/* <Route path="/day/:dayId" component={DayDetails} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/views/TenDaysForecast.jsx
@@ -0,0 +1,16 @@
+// import Clima from "../Clima"
+// import Stadistics from "../DashboardStas"
+// import Header from "../Header"
+
+const TenDaysForecast = () => {
+    return (
+        <div className="bg-[#f6edff]">
+            {/* <main className="mx-auto container ">
+                <Header />
+                <Stadistics />
+                <Clima />
+            </main> */}
+        </div>
+    )
+}
+export default TenDaysForecast
